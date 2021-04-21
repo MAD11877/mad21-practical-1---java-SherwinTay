c0c484a513a057b2c5fab2bfdb3f34ac64ea4539
SherwinTay <82798383+SherwinTay@users.noreply.github.com>
2021-04-21 10:15:57 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import static java.util.Arrays.*;
 
 public class Question5
 {
@@ -28,5 +30,40 @@ public class Question5
      
     Scanner in = new Scanner(System.in);
     
+    // For input
+    int occurance = in.nextInt();
+    ArrayList<Integer> data = new ArrayList<>();
+
+    for (int i = 0; i < occurance; i ++)
+    {
+      int adder = in.nextInt();
+      data.add(adder);
+    }
+
+
+    // For checking and counting
+    int maxnumber = 0;
+    int count = 0;
+    int maxcount;
+
+    for (int num : data)
+    {
+        
+        for (int check : data)
+        {
+          if (check == num)
+          {
+           count++;
+          }
+        }
+
+        if (count > maxnumber)
+        {
+          maxnumber = num;
+          maxcount = count;
+        }
+        
+    }
+    System.out.println(maxnumber);
   }
 }
